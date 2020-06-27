ab3d3b5c1d7dd6f8383f757c9a57ef008e57e644
Jackson311 <448545478@qq.com>
2020-06-27 16:17:15 +0800

    新增撤回重做操作

--- Command/Controller.cs
+++ Command/Controller.cs
@@ -1,16 +1,21 @@
-﻿namespace Command
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Command
 {
     public class Controller
     {
         // 开关命令对象
         private ICommand _switchCommand;
+
         // 频道命令对象
         private ICommand _channleCommand;
+
         // 音量命令对象
         private ICommand _volumeCommand;
-        
+
         /*绑定函数*/
-        
+
         /// <summary>
         /// 绑定开关命令对象
         /// </summary>
@@ -37,7 +42,7 @@
         {
             _volumeCommand = command;
         }
-        
+
         /*执行命令*/
 
         public void SwitchCommandExe()
@@ -68,6 +73,40 @@
         public void VolumeCommandUnExe()
         {
             _volumeCommand.UnExe();
+        }
+    }
+
+    public class Commander
+    {
+        private List<ICommand> _commands = new List<ICommand>();
+        private int currentCommandIndex = -1;
+
+        public void AddCommand(ICommand command)
+        {
+            command.Exe();
+            _commands.Add(command);
+
+            ++currentCommandIndex;
+        }
+
+        public void Do()
+        {
+            // 重做命令处于顶部
+            if (currentCommandIndex == _commands.Count - 1)
+                return;
+
+            ++currentCommandIndex;
+            _commands[currentCommandIndex].Exe();
+        }
+
+        public void UnDo()
+        {
+            if (currentCommandIndex < 0)
+                return;
+                
+
+            _commands[currentCommandIndex].UnExe();
+            --currentCommandIndex;
         }
     }
 }
--- Command/Program.cs
+++ Command/Program.cs
@@ -116,7 +116,88 @@ namespace Command
             Console.WriteLine("收音机音量，下");
         }
     }
-    
+
+    class Person
+    {
+        private int _x;
+        private int _y;
+
+        public void Move(int x,int y)
+        {
+            _x = x;
+            _y = y;
+            Console.WriteLine("Person Walk x:{0} ,y:{1}",_x,_y);
+        }
+
+        public void GetPostion(out int x, out int y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+
+    class JumpPerson
+    {
+        private int _z;
+
+        public void Jum(int z)
+        {
+            _z = z;
+            Console.WriteLine("Person Jump z:{0}",_z);
+        }
+
+        public int Z => _z;
+    }
+    class MoveCommnad : ICommand
+    {
+        private Person _person;
+        private int _oriX;
+        private int _oriY;
+        private int _curX;
+        private int _curY;
+
+        public MoveCommnad(Person person, int x, int y)
+        {
+            _person = person;
+            _curX = x;
+            _curY = y;
+            _person.GetPostion(out _oriX,out _oriY);
+        }
+        
+        public void Exe()
+        {
+            _person.Move(_curX,_curY);
+        }
+
+        public void UnExe()
+        {
+            _person.Move(_oriX,_oriY);
+        }
+    }
+
+    class JumoCommand : ICommand
+    {
+        private JumpPerson _jumpPerson;
+        private int _oriZ;
+        private int _curZ;
+
+        public JumoCommand(JumpPerson jumpPerson,int z)
+        {
+            _jumpPerson = jumpPerson;
+            _curZ = z;
+            _oriZ = jumpPerson.Z;
+        }
+
+        public void Exe()
+        {
+            _jumpPerson.Jum(_curZ);
+        }
+
+        public void UnExe()
+        {
+            _jumpPerson.Jum(_oriZ);
+        }
+    }
     internal class Program
     {
         public static void Main(string[] args)
@@ -127,21 +208,33 @@ namespace Command
              * 只要命令里有去实现客户端的就行，而且你可以实现非常多不同的命令，同时还支持撤回等操作
              */
             
-            TV tv = new TV();
-            Radio radio = new Radio();
-            Controller controller = new Controller();
-            
-            controller.BindSwitchCommand(new SwitchCommand(radio));
-            controller.BindChannleCommand(new ChannleCommand(radio));
-            controller.BindVolumeCommnad(new VolumeCommand(radio));
-            
-            controller.SwitchCommandExe();
-            controller.SwitchCommandUnExe();
-            controller.ChannleCommnadExe();
-            controller.ChannleCommnadUnExe();
-            controller.VolumeCommandExe();
-            controller.VolumeCommandUnExe();
-            
+            Person person = new Person();
+            JumpPerson jumpPerson = new JumpPerson();
+
+            Commander commander = new Commander();
+            commander.AddCommand(new MoveCommnad(person,1,1));
+            commander.AddCommand(new JumoCommand(jumpPerson,1));
+            commander.AddCommand(new MoveCommnad(person,2,1));
+            commander.AddCommand(new JumoCommand(jumpPerson,3));
+            commander.AddCommand(new MoveCommnad(person,2,2));
+            commander.AddCommand(new JumoCommand(jumpPerson,5));
+            commander.AddCommand(new JumoCommand(jumpPerson,6));
+            Console.WriteLine("----------撤销");
+            commander.UnDo();
+            commander.UnDo();
+            commander.UnDo();
+            commander.UnDo();
+            commander.UnDo();
+            commander.UnDo();
+            commander.UnDo();
+            Console.WriteLine("----------恢复");
+            commander.Do();
+            commander.Do();
+            commander.Do();
+            commander.Do();
+            commander.Do();
+            commander.Do();
+
         }
     }
 }
