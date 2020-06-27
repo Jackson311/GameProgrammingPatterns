4c2ccdd34995a949bfa38ce0dfe401f63d75390d
Jackson311 <448545478@qq.com>
2020-06-27 13:57:15 +0800

    客户端-控制器

--- Command/Program.cs
+++ Command/Program.cs
@@ -2,6 +2,28 @@
 
 namespace Command
 {
+    /// <summary>
+    /// 控制器
+    /// </summary>
+    class Switcher
+    {
+        private ISwitchable _switchable;
+
+        public Switcher(ISwitchable switchable)
+        {
+            _switchable = switchable;
+        }
+
+        public void OnButton()
+        {
+            _switchable.On();
+        }
+
+        public void OffButton()
+        {
+            _switchable.Off();
+        }
+    }
 
     class Bulb : ISwitchable
     {
@@ -33,12 +55,19 @@ namespace Command
     {
         public static void Main(string[] args)
         {
+            /*
+             * 现在是客户端-控制器
+             * 控制器获取要控制的客户端，然后进行控制
+             */
             Bulb bulb = new Bulb();
-            bulb.On();
-            bulb.Off();
             Fan fan = new Fan();
-            fan.On();
-            fan.Off();
+            Switcher switcher_0 = new Switcher(bulb);
+            switcher_0.OnButton();
+            switcher_0.OffButton();
+            Switcher switcher_1 = new Switcher(fan);
+            switcher_1.OnButton();
+            switcher_1.OffButton();
+
         }
     }
 }
