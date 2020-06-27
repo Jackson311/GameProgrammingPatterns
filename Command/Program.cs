33a27c54ac9bbd23baaa93abd7084d85bc95764f
Jackson311 <448545478@qq.com>
2020-06-27 13:49:40 +0800

    简单实现了开关

--- Command/Program.cs
+++ Command/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace Command
+﻿using System;
+
+namespace Command
 {
+
+    class Bulb : ISwitchable
+    {
+        public void On()
+        {
+            Console.WriteLine("通电，亮灯");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("断电，关灯");
+        }
+    }
+
+    class Fan : ISwitchable
+    {
+        public void On()
+        {
+            Console.WriteLine("通电，开风扇");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("断电，关风扇");
+        }
+    }
+    
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Bulb bulb = new Bulb();
+            bulb.On();
+            bulb.Off();
+            Fan fan = new Fan();
+            fan.On();
+            fan.Off();
         }
     }
 }
---
+++ Command/Switchable.cs
@@ -0,0 +1,15 @@
+﻿namespace Command
+{
+    public interface ISwitchable
+    {
+        /// <summary>
+        /// 开动作
+        /// </summary>
+        void On();
+
+        /// <summary>
+        /// 关动作
+        /// </summary>
+        void Off();
+    }
+}
